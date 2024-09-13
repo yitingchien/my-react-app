16930a31e89dcd5423df1c3bcc27b5c89714cf08
Yi-Ting Chien <111419056+yitingchien@users.noreply.github.com>
2024-09-13 14:41:43 +0200

    CSS grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn React</h1>
-    <p>Welcome to your new site by react.</p>
-    <Link to="/page-2/">Go to page 2 video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn React</h1>
+        <p>Welcome to your new site by react. Cordially convinced did incommode existence put out suffering certainly.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+    
   </div>
 )
 
