5f1ba83a7047151423fed5ac71824b27048db895
Yi-Ting Chien <111419056+yitingchien@users.noreply.github.com>
2024-09-13 12:01:37 +0200

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn React</h1>
+    <p>Welcome to your new site by react.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
