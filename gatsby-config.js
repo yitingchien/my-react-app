35a8ccd3969df40a091ab85bbd6058de7c21e2fd
Yi-Ting Chien <111419056+yitingchien@users.noreply.github.com>
2024-09-18 17:01:12 +0200

    Remove contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,15 @@ module.exports = {
     description: 'To reduce the number of requests to the server, importing images that are less than 10,000 bytes returns a data URI instead of a path. This applies to the following file extensions: bmp, gif, jpg, jpeg, and png. SVG files are excluded due to #1153.',
     keywords: 'design, code, react, arkit'
   },
-  // plugins: [
-  //   'gatsby-plugin-react-helmet',
-  //   {
-  //     resolve: 'gatsby-source-contentful',
-  //     option: {
-  //       spaceId: 'balscd90u3hm',
-  //       accessToken: process.env.km_qF94Mw88i-ay_mSdemXfBIxUZ9th8eDd5IvMpR-U,
-  //     },
-  //   },
-  //   `gatsby-plugin-image`,
-  // ],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   option: {
+    //     spaceId: 'balscd90u3hm',
+    //     accessToken: process.env.km_qF94Mw88i-ay_mSdemXfBIxUZ9th8eDd5IvMpR-U,
+    //   },
+    // },
+    // `gatsby-plugin-image`,
+  ]
 }
