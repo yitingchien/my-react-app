b31d00c62c1d81a357366fb420301ccf60111e29
Yi-Ting Chien <111419056+yitingchien@users.noreply.github.com>
2024-09-18 14:09:23 +0200

    connect to contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'To reduce the number of requests to the server, importing images that are less than 10,000 bytes returns a data URI instead of a path. This applies to the following file extensions: bmp, gif, jpg, jpeg, and png. SVG files are excluded due to #1153.',
+    keywords: 'design, code, react, arkit'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      option: {
+        spaceId: 'balscd90u3hm',
+        accessToken: 'km_qF94Mw88i-ay_mSdemXfBIxUZ9th8eDd5IvMpR-U'
+      }
+    }
+  ],
 }
