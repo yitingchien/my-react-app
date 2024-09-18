d2b5e9a588ee893f036fba51d4b1b84487d2c108
Yi-Ting Chien <111419056+yitingchien@users.noreply.github.com>
2024-09-18 17:04:25 +0200

    Update wave to Wave

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Wave from './wave'
+import Wave from './Wave'
 
 const SectionGroup = styled.div`
     background: url(${props => props.image});
